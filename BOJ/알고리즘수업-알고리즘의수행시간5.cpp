6786f0d6e4f65dba765e081216c0e973037f0bef
u0empty <yubeen00@naver.com>
2024-11-02 14:56:46 +0900

    solved 알고리즘수업-알고리즘의수행시간 2020ㅏㅠ 0ㅡㄴ

---
+++ BOJ/알고리즘수업-알고리즘의수행시간5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	long long n;
+	cin >> n;
+	cout << fixed << setprecision(0) << n * n * n << "\n" << 3;
+
+	return 0;
+}
