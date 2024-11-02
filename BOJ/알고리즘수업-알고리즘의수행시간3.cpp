8f4388df23fb270c17838dc6c976b64df14e4fca
u0empty <yubeen00@naver.com>
2024-11-02 14:13:49 +0900

    solved 알고리즘수업-알고리즘의수행시간3 2020kb 0ms

---
+++ BOJ/알고리즘수업-알고리즘의수행시간3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	cout << fixed << setprecision(0) << pow(n, 2) << '\n' << 2;
+	return 0;
+}
