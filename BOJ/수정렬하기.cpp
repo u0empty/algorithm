f071bbc134cf1f4d543ff6f1a670f57e8d5a7300
u0empty <yubeen00@naver.com>
2024-11-13 15:41:16 +0900

    solved 수정렬하기 2060ms 0kb 2m]

---
+++ BOJ/수정렬하기.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int num[10001] = { 0, };
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+	sort(num, num + n);
+	for (int i = 0; i < n; i++) {
+		cout << num[i] << "\n";
+	}
+
+	return 0;
+}
