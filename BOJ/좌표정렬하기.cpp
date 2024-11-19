47c257cc4feb455e21ea2215e1ab8481d9aad9e6
u0empty <yubeen00@naver.com>
2024-11-19 18:05:08 +0900

    solved 좌표정렬하기 3572kb 104ms 3m

--- BOJ/좌표정렬하기.cpp
+++ BOJ/좌표정렬하기.cpp
@@ -4,37 +4,20 @@
 
 using namespace std;
 
-int n;
-vector<vector<int>> posV(100001);
-vector<vector<int>> negV(100001);
+int n, x, y;
+vector<pair<int, int>> v;
 
 int main() {
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		int x, y;
 		cin >> x >> y;
-		if (x < 0) negV[-x].push_back(y);
-		else posV[x].push_back(y);
+		v.push_back({ x, y });
 	}
 
-	for (int i = 0; i < 100001; i++) {
-		sort(posV[i].begin(), posV[i].end());
-	}
-	for (int i = 0; i < 100001; i++) {
-		sort(negV[i].begin(), negV[i].end());
-	}
+	sort(v.begin(), v.end());
 
-	for (int i = 100000; i > 0; i--) {
-		if (negV[i].empty()) continue;
-		for (int j = 0; j < negV[i].size(); j++) {
-			cout << -i << " " << negV[i][j] << "\n";
-		}
-	}
-	for (int i = 0; i < 100001; i++) {
-		if (posV[i].empty()) continue;
-		for (int j = 0; j < posV[i].size(); j++) {
-			cout << i << " " << posV[i][j] << "\n";
-		}
+	for (int i = 0; i < n; i++) {
+		cout << v[i].first << " " << v[i].second << "\n";
 	}
 
 	return 0;
