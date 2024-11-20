39a7e3125620a65c8d8fa876605104ad4d3c0a85
u0empty <yubeen00@naver.com>
2024-11-20 15:25:16 +0900

    solved 단어정렬 4532ms 68kb

--- BOJ/단어정렬.cpp
+++ BOJ/단어정렬.cpp
@@ -23,9 +23,8 @@ int main() {
 		cin >> s;
 		v.push_back(s);
 	}
-	sort(v.begin(), v.end());
-	v.erase(unique(v.begin(), v.end()), v.end());
 	sort(v.begin(), v.end(), cmp);
+	v.erase(unique(v.begin(), v.end()), v.end());
 	
 	for (int i = 0; i < v.size(); i++) {
 		cout << v[i] << "\n";
