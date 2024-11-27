13cd637435e991d4ec989c9e600e7d63a4fb0415
u0empty <yubeen00@naver.com>
2024-11-27 23:16:31 +0900

    solved 서로다른부분문자열의개수 211908kb 848mb 54m

--- BOJ/서로다른부분문자열의개수.cpp
+++ BOJ/서로다른부분문자열의개수.cpp
@@ -9,17 +9,11 @@ set<string> s;
 int main() {
 	cin >> str;
 
-	string tmp;
 	for (int i = 1; i <= str.length(); i++) {
 		for (int j = 0; j <= str.length() - i; j++) {
-			for (int k = j; k < j + i; k++) {
-				tmp += str[k];
-			}
-			s.insert(tmp);
-			tmp = "";
+			s.insert(str.substr(j, i));
 		}
 	}
-
 	cout << s.size();
 
 	return 0;
