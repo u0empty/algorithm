17565380f9f2b50ee846077bcaaec2cc62b314ab
u0empty <yubeen00@naver.com>
2024-12-14 23:18:38 +0900

    solved 인사성밝은곰곰이 8760kb 96ms

--- BOJ/인사성밝은곰곰이.cpp
+++ BOJ/인사성밝은곰곰이.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
+#include <set>
 #include <string>
-#include <unordered_map>
 using namespace std;
 
 int main() {
-	string str;
-	int n;
-	
-	cin >> n;
-	unordered_map<string, int> m(n);
+    int n;
+    cin >> n;
 
-	int cnt = 0;
-	for (int i = 0; i < n; i++) {
-		cin >> str;
-		if (str == "ENTER") {
-			m.clear();
-		}
-		else {
-			m[str]++;
-			if (m[str] == 1) cnt++;
-		}
-	}
-	cout << cnt;
+    set<string> s;
+    int cnt = 0;
 
-	return 0;
+    for (int i = 0; i < n; i++) {
+        string str;
+        cin >> str;
+
+        if (str == "ENTER") {
+            cnt += s.size();
+            s.clear();
+        }
+        else {
+            s.insert(str);
+        }
+    }
+    cnt += s.size();
+
+    cout << cnt;
+    return 0;
 }
