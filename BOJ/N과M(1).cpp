9ce6258b40569c69fe23722bf87bd4999f2951d5
u0empty <yubeen00@naver.com>
2024-12-21 18:11:28 +0900

    solved N과M(1) 2020kb 24ms

---
+++ BOJ/N과M(1).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+int arr[9];
+int visit[9];
+vector<int> v;
+
+void solution(int lev) {
+	if (lev == M) {
+		for (int i = 0; i < v.size(); i++) cout << v[i] << " ";
+		cout << "\n";
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		if (visit[i]) continue;
+		visit[i] = 1;
+		v.push_back(arr[i]);
+		solution(lev + 1);
+		visit[i] = 0;
+		v.pop_back();
+	}
+}
+
+int main() {
+	 cin >> N >> M;
+	for (int i = 1; i <= N; i++) arr[i] = i;
+	solution(0);
+}
