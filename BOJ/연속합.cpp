d3955f3c737254b51a10eace4ebe08603359f10c
u0empty <yubeen00@naver.com>
2025-01-22 17:51:17 +0900

    solved 연속합 2680kb 8ms

--- BOJ/연속합.cpp
+++ BOJ/연속합.cpp
@@ -6,28 +6,18 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n;
-	int maxi = -1e9;
 
+	int n, dp[100000];
 	cin >> n;
-	vector<int> v(n);
-	for (int i = 0; i < n; i++) {
-		cin >> v[i];
-		maxi = max(maxi, v[i]);
-	}
 
-	int idx = 1;
-	vector<int> v1 = v, v2;
-	while (v1.size() > 1) {
-		for (int i = idx; i < n; i++) {
-			v2.push_back(v1[i - idx] + v[i]);
-		}
-		for (int i = 0; i < v2.size(); i++) {
-			maxi = max(maxi, v2[i]);
-		}
-		v1 = v2;
-		v2.clear();
-		idx++;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) cin >> v[i];
+
+	int maxi = v[0];
+	dp[0] = v[0];
+	for (int i = 1; i < n; i++) {
+		dp[i] = max(dp[i - 1] + v[i], v[i]);
+		maxi = max(dp[i], maxi);
 	}
 	cout << maxi;
 	return 0;
