d992f8c1b50c8f0f7648fe8a2f6a59c603861cb9
u0empty <yubeen00@naver.com>
2025-02-03 00:08:25 +0900

    solved 쉬운계단수 2024kb 0ms

--- BOJ/쉬운계단수.cpp
+++ BOJ/쉬운계단수.cpp
@@ -1,1 +1,29 @@
- 
+#include <iostream>
+using namespace std;
+
+#define N 1000000000
+
+int n;
+int dp[101][10];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n;
+	for (int i = 1; i <= 9; i++) {
+		dp[1][i] = 1;
+	}
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j <= 9; j++) {
+			if (j == 0) dp[i][j] = dp[i - 1][1] % N;
+			else if (j == 9) dp[i][j] = dp[i - 1][8] % N;
+			else dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % N;
+		}
+	}
+	long long sum = 0;
+	for (int i = 0; i <= 9; i++) {
+		sum += dp[n][i];
+	}
+	cout << sum % N;
+	return 0;
+} 
