4d8b1c1ee069d21a71631be72ee925cf488beaf5
u0empty <yubeen00@naver.com>
2025-02-05 00:48:09 +0900

    solved 포도주시식

--- BOJ/포도주시식.cpp
+++ BOJ/포도주시식.cpp
@@ -1,1 +1,23 @@
- 
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int gr[10001];
+int dp[10001];
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> gr[i];
+
+	dp[0] = gr[0];
+	dp[1] = gr[0] + gr[1];
+	dp[2] = max({ dp[1], gr[0] + gr[2], gr[1] + gr[2] });
+	for (int i = 3; i < n; i++)
+		dp[i] = max({ dp[i - 1], dp[i - 3] + gr[i - 1] + gr[i], dp[i - 2] + gr[i] });
+
+	cout << dp[n - 1];
+
+	return 0;
+}
