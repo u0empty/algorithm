4fee9b80ea412e53fcb0c9383455c466ec02c36d
u0empty <yubeen00@naver.com>
2025-03-02 19:17:31 +0900

    solved 수찾기(2) 2416kb 44ms

--- BOJ/수찾기(2).cpp
+++ BOJ/수찾기(2).cpp
@@ -1,1 +1,36 @@
- 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n, m, tmp;
+vector<int> v;
+
+int solution(int num) {
+	int st = 0;
+	int en = n - 1;
+	int md;
+	while (st <= en) {
+		md = (st + en) / 2;
+		if (v[md] == num) return 1;
+		else if (v[md] < num) st = md + 1;
+		else en = md - 1;
+	}
+	return 0;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n;
+	v.resize(n);
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+	sort(v.begin(), v.end());
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> tmp;
+		cout << solution(tmp) << "\n";
+	}
+	return 0;
+}
