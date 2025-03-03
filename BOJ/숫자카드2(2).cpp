f8690af7109d3233762303ee34005864d8a886e7
u0empty <yubeen00@naver.com>
2025-03-03 20:32:53 +0900

    solving 랜선자르기

--- BOJ/숫자카드2(2).cpp
+++ BOJ/숫자카드2(2).cpp
@@ -1,1 +1,23 @@
- 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, m, tmp;
+	cin >> n;
+	vector<int> v(20000001);
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		v[tmp + 10000000]++;
+	}
+
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> tmp;
+		cout << v[tmp + 10000000] << " ";
+	}
+	return 0;
+}
