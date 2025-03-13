b9c84ce20ca4b6296a4098889dff7486b75bce68
u0empty <yubeen00@naver.com>
2025-03-13 17:18:29 +0900

    solved K번째수 2020kb 28ms

--- BOJ/K번째수.cpp
+++ BOJ/K번째수.cpp
@@ -1,1 +1,36 @@
- 
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+long long N, K, ans;
+
+void solution() {
+	long long l = 1;
+	long long r = N * N;
+	long long m, cnt;
+	while (l <= r) {
+		m = (l + r) / 2;
+		cnt = 0;
+		for (long long i = 1; i <= N; i++)
+			cnt += min(m / i, N);
+		if (cnt < K) {
+			l = m + 1;
+		}
+		else {
+			ans = m;
+			r = m - 1;
+		}
+	}
+	return;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> K;
+	solution();
+	cout << ans;
+
+	return 0;
+}
