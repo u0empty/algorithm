9375085d13da6a0a2e7d51f16208f634fab0886e
u0empty <yubeen00@naver.com>
2025-03-20 15:41:43 +0900

    solved 절댓값힙 2532kb 12ms

--- BOJ/절댓값힙.cpp
+++ BOJ/절댓값힙.cpp
@@ -1,1 +1,35 @@
- 
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+struct cmp {
+	bool operator()(int a, int b) {
+		if (abs(a) == abs(b)) return a > b;
+		else return abs(a) > abs(b);
+	}
+};
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, num;
+	cin >> n;
+	priority_queue<int, vector<int>, cmp> pq;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		if (num == 0) {
+			if (pq.empty()) cout << "0\n";
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(num);
+		}
+	}
+
+	return 0;
+}
