8cb421620e8cfd8a9c8fe9bf18de8c40ea4124a3
u0empty <yubeen00@naver.com>
2025-04-04 19:44:30 +0900

    improved 음료수얼려먹기(BFS)

--- 이코테/음료수얼려먹기(BFS).cpp
+++ 이코테/음료수얼려먹기(BFS).cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 #include <string>
 using namespace std;
 
 vector<string> v(4);
-bool visited[4][5];
-int cnt = 0;
+queue<pair<int, int>> q;
 int dy[4] = { -1, 1, 0, 0 };
 int dx[4] = { 0, 0, -1, 1 };
-queue<pair<int, int>> q;
+int cnt = 0;
 
-void bfs(int y, int x) {
+bool bfs(int y, int x) {
+	if (v[y][x] == '1') return false;
+
 	q.push({ y, x });
-	visited[y][x] = true;
+	v[y][x] = '1';
 
 	while (!q.empty()) {
 		int nowy = q.front().first;
@@ -23,26 +24,22 @@ void bfs(int y, int x) {
 		for (int i = 0; i < 4; i++) {
 			int ny = nowy + dy[i];
 			int nx = nowx + dx[i];
-
-			if (visited[ny][nx]) continue;
+			
 			if (ny < 0 || nx < 0 || ny >= 4 || nx >= 5) continue;
 			if (v[ny][nx] == '1') continue;
-
+			
 			q.push({ ny, nx });
-			visited[ny][nx] = true;
+			v[ny][nx] = '1';
 		}
 	}
+	return true;
 }
 
 int main() {
 	for (int i = 0; i < 4; i++) cin >> v[i];
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 5; j++) {
-			if (v[i][j] == '1') continue;
-			if (!visited[i][j]) {
-				bfs(i, j);
-				cnt++;
-			}
+			if (bfs(i, j)) cnt++;
 		}
 	}
 	cout << cnt;
