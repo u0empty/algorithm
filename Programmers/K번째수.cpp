33f6c4d029777a1d0c2e1ee307b09c4c3c510973
u0empty <yubeen00@naver.com>
2025-04-09 22:38:30 +0900

    solved K번째수

--- Programmers/K번째수.cpp
+++ Programmers/K번째수.cpp
@@ -1,1 +1,25 @@
-나는왜이럴가
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer, v;
+    
+    for (int i = 0; i < commands.size(); i++) {
+        int a = commands[i][0];
+        int b = commands[i][1];
+        int c = commands[i][2];
+        
+        for (int k = a - 1; k < b; k++) {
+            v.push_back(array[k]);
+        }
+        
+        sort(v.begin(), v.end());
+        answer.push_back(v[c - 1]);
+        v.clear();
+    }
+    
+    return answer;
+}
