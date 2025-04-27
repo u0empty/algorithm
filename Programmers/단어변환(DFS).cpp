ea2cef4663b99ef6aa3503b79ec89b80824469c3
u0empty <yubeen00@naver.com>
2025-04-27 23:57:07 +0900

    modified 단어변환(DFS)

--- Programmers/단어변환(DFS).cpp
+++ Programmers/단어변환(DFS).cpp
@@ -1,12 +1,11 @@
 #include <algorithm>
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-int ans = 1e9;
-vector<bool> used;
+vector<bool> visited;
+int answer = 1e9;
 
 bool is1Diff(string a, string b) {
     int diff = 0;
@@ -16,18 +15,18 @@ bool is1Diff(string a, string b) {
     return diff == 1;
 }
 
-void dfs(string now, string tar, vector<string>& words, int cnt) {
-    if (now == tar) {
-        ans = min(ans, cnt);
+void dfs(string now, string target, vector<string> words, int cnt) {
+    if (now == target) {
+        answer = min(answer, cnt);
         return;
     }
     
     for (int i = 0; i < words.size(); ++i) {
-        if (used[i]) continue;
+        if (visited[i]) continue;
         if (is1Diff(now, words[i])) {
-            used[i] = true;
-            dfs(words[i], tar, words, cnt + 1);
-            used[i] = false;
+            visited[i] = true;
+            dfs(words[i], target, words, cnt + 1);
+            visited[i] = false;
         }
     }
 }
@@ -37,9 +36,9 @@ int solution(string begin, string target, vector<string> words) {
     int n = words.size();
     if (ti == n) return 0;
     
-    used.assign(n, false);
+    visited.assign(n, false);
     
     dfs(begin, target, words, 0);
     
-    return ans == 1e9 ? 0 : ans;
+    return answer == 1e9 ? 0 : answer;
 }
